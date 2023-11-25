9560a36125e9fb772221a10991f02500ac6b6246
黄楚才 <chucaihuang@kugou.net>
2023-11-25 17:30:59 +0800

    chore(maven): 初始化springboot配置

---
+++ src/main/java/accounting/controller/HelloContainer.java
@@ -0,0 +1,19 @@
+package accounting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloContainer {
+    @GetMapping(path = "v1/hello/{name}/{id}")
+    public String sayHello(@PathVariable String name, @PathVariable Integer id) {
+        return String.format("Hello, %s, %d", name, id);
+    }
+
+    @GetMapping(path = "v1/greeting")
+    public String sayGreetings(@RequestParam String name) {
+        return String.format("Greetings, %s", name);
+    }
+}
