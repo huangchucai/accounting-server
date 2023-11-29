e0d87d44eb800500d99adaa5777d8cbd06c04fef
黄楚才 <chucaihuang@kugou.net>
2023-11-29 21:00:28 +0800

    feat: 新增用户Controller的接口文档

--- src/main/java/com/hcc/accounting/Application.java
+++ src/main/java/com/hcc/accounting/Application.java
@@ -1,9 +1,12 @@
 package com.hcc.accounting;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "山竹记账服务端", version = "v1", description = "山竹记账服务端接口文档"))
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ src/main/java/com/hcc/accounting/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.hcc.accounting.controller;
+
+import com.hcc.accounting.mapper.UserInfoMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "用户相关接口", description = "用户管理的相关接口")
+public class UserController {
+
+    UserInfoMapper userInfoMapper;
+
+    @Autowired
+    public UserController(UserInfoMapper userInfoMapper) {
+        this.userInfoMapper = userInfoMapper;
+    }
+
+    @GetMapping(path = "v1/users/{id}")
+    @Operation(summary = "根据用户id获取用户信息", description = "返回相应的用户信息",
+               responses = {@ApiResponse(responseCode = "200", description = "获取用户信息成功"), @ApiResponse(
+                       responseCode = "404", description = "用户不存在")})
+    public com.hcc.accounting.model.UserInfo getUserInfoById(
+            @Parameter(description = "具体的用户id") @PathVariable Long id) {
+        return userInfoMapper.getUserInfoById(id);
+    }
+}
